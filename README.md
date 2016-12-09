535176c4b8b2f614c50823c10672815ed248a709
fyzxs <Fyzxs@users.noreply.github.com>
2016-12-08 20:51:14 -0800

    Bulk Load

---
+++ README.md
@@ -0,0 +1,4 @@
+# FyzLog
+My Android Log Wrapper.
+
+A simplified logger, and enables unit tests of methods that log.
---
+++ new/TheFyzLogTests.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright (c) 2016 Fyzxs. MIT License
+ */
+
+package com.fyzxs.android.log;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.contrib.java.lang.system.SystemOutRule;
+import org.junit.rules.ExpectedException;
+
+import static junit.framework.Assert.fail;
+import static org.assertj.core.api.Java6Assertions.assertThat;
+
+@SuppressWarnings("ConstantConditions")
+public class TheFyzLogTests {
+
+    @Rule
+    public final SystemOutRule systemOutRule = new SystemOutRule().enableLog();
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
+
+    @Test
+    public void systemOut() throws Exception {
+        FyzLog.logToSystem();
+
+        systemOutRule.clearLog();
+        final Thread thread = new Thread(() -> {
+            FyzLog.wtf("Non formatted!");
+        });
+        thread.setName("CustomName");
+        thread.start();
+        thread.join();
+        assertThat(systemOutRule.getLog()).isNotEmpty();
+
+    }
+
+    @Test
+    public void androidLog() throws Exception {
+
+        try {
+            FyzLog.wtf("Non formatted!");
+            fail("Shouldn't get here");
+        } catch (final RuntimeException e) {
+            assertThat(e.getMessage()).isEqualTo(
+                    "Method wtf in android.util.Log not mocked. See http://g.co/androidstudio/not-mocked for details.");
+        }
+    }
+}
